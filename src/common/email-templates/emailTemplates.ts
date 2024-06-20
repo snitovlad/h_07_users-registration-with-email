ac866905ce6ee03924da94a8a17f41b7a7bfaee9
snitovlad <snitovov@gmail.com>
2024-06-20 19:01:42 +0300

    Fixed link of message

--- src/common/email-templates/emailTemplates.ts
+++ src/common/email-templates/emailTemplates.ts
@@ -1,13 +1,13 @@
 export const emailTemplates = {
 
-    registrationEmail(confirmationCode: string): string {
-        const message = `
+  registrationEmail(confirmationCode: string): string {
+    const message = `
           <h1>Thank you for your registration</h1>
           <p>
             To finish registration please follow the link below:
-            <a href={"https://somesite.com/confirm-email?code=${confirmationCode}"}>complete registration</a>
+            <a href="https://somesite.com/confirm-email?code=${confirmationCode}">complete registration</a>
           </p>
     `
-        return message
-    }
+    return message
+  }
 }
